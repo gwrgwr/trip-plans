747779fb3c9812bb126e5fa53db9b3b7334b3ae2
gwrgwr <murilomassa72@gmail.com>
2024-10-04 15:05:45 -0300

    first commit

---
+++ src/main/java/br/com/murilo/tripplans/TripPlansApplication.java
@@ -0,0 +1,13 @@
+package br.com.murilo.tripplans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripPlansApplication.class, args);
+    }
+
+}
